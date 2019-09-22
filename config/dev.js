054c318bdac47d997391d26f4f7625bcfb7141f3
akoDev <ako@Adas-MacBook-Pro.local>
2019-09-22 23:04:10 +0800

    lecture 39 updated mongoose

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
-module.exports = {
+  module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb+srv://<usename>:<password>@cluster0-1lxde.mongodb.net/test?retryWrites=true&w=majority',
   cookieKey: '123123123'
 };
