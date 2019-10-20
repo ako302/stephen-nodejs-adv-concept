0485c17cca83bb29a9a572a9ab1432e3ccd29f74
AKO <akodev302@gmail.com>
2019-10-20 13:51:24 +0800

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb+srv://advnode:advnode@cluster0-1lxde.mongodb.net/test?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://<user>:<pw>@cluster0-1lxde.mongodb.net/test?retryWrites=true&w=majority',
   cookieKey: '123123123', // client secret, used to generate session.sig to ensure the session is not tampered
   //keygrip in cookies module generates the session.sig
   redisUrl: 'redis://127.0.0.1:6379'
